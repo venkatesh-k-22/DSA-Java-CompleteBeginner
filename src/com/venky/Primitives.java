853c75b9d68494c0bbd135c09ccf4bb0f57f974d
Venkatesh K <venkateshk14046@gmail.com>
2025-12-24 10:16:27 +0530

    Java Primitive data types.

---
+++ src/com/venky/Primitives.java
@@ -0,0 +1,16 @@
+package com.venky;
+
+public class Primitives {
+    public static void main(String[] args) {
+        //a is a Identifier 67 is a literal
+        //In primitive data types we don't have inbuilt functions to perform some operations.
+        int a = 67;
+        //c is a literal
+        char ch = 'c';
+        //we need to add f at the end of floating values to represent, it is a floating point datatype
+        float fl = 34567.78f;
+        double db = 7897826732863296.89;
+        //we need to add L at the end of long type variables to represent it is a long type variable
+        long ln = 3334534678688897897L;
+    }
+}
