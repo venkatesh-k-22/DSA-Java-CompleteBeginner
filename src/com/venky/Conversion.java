bb47195213617533e98cf473b1583eb9b6f90f66
Venkatesh K <venkateshk14046@gmail.com>
2025-12-24 22:12:31 +0530

    Type casting, conversion in java

---
+++ src/com/venky/Conversion.java
@@ -0,0 +1,30 @@
+package com.venky;
+
+import java.util.Scanner;
+
+public class Conversion {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        int num1 = input.nextInt();
+//        int num2 = input.nextInt();
+//        //automatic type promotion
+//        float f = num1+num2;
+        // we cant convert float to int automatically
+//        System.out.println(f);
+
+        //if we want explicit type casting we can use ()
+//        int a = (int)(67.87);
+//        System.out.println(a);
+
+        // lets check for byte byte size is 256
+        byte a = 40;
+        byte b = 50;
+        byte c = 100;
+        //while performing operation automatically java converts from byte to int
+        //a and  b is byte first it will get 2000 but the operation will take into int
+        int res = (a*b)/c; // 20
+        System.out.println(res);
+
+
+    }
+}
